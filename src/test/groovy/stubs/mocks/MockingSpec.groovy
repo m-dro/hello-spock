ed131b22d6b82b91000e73a99d72fa2367f96a8d
Mirek Drozd <mirek.drozd@mail.com>
2023-07-01 06:31:03 +0200

    JTWS - Mocking - add cool comments

--- src/test/groovy/stubs/mocks/MockingSpec.groovy
+++ src/test/groovy/stubs/mocks/MockingSpec.groovy
@@ -20,7 +20,12 @@ class MockingSpec extends Specification {
          tv = new Product(name: "bravia", price: 1200, weight: 12)
     }
 
-
+    /**
+     * <div style="font-size: 30;background:#3c423f"><div style="font-size: 35;">&#128142 Using Mocks</div>
+     * <br><ul>
+     * <li>Testing how many times a method was invoked</li>
+     * </ul>
+     */
     def "credit card connection is always closed down"(){
         given: "credit card service"
         CreditCardProcessor service = Mock(CreditCardProcessor)
@@ -34,6 +39,12 @@ class MockingSpec extends Specification {
         1 * service.shutdown()
     }
 
+    /**
+     * <div style="font-size: 30;background:#3c423f"><div style="font-size: 35;">&#128142 Using Mocks</div>
+     * <br><ul>
+     * <li>Testing specific order of method invokation with multiple then blocks</li>
+     * </ul>
+     */
     def "credit card connection is closed down as last operation"(){
         given: "credit card service"
         CreditCardProcessor service = Mock(CreditCardProcessor)
@@ -50,6 +61,14 @@ class MockingSpec extends Specification {
         1 * service.shutdown()
     }
 
+    /**
+     * <div style="font-size: 30;background:#3c423f"><div style="font-size: 35;">&#128142 Using Mocks</div>
+     * <br><ul>
+     * <li>Testing how many times specific methods were invoked</li>
+     * <li>Stubbing what specific methods should return</li>
+     * <li>Testing that no other methods were invoked</li>
+     * </ul>
+     */
     def "warehouse is queried for each product - strict"() {
         given: "camera and warehouse with limitless stock"
         Product camera = new Product(name: "panasonic", price: 350, weight: 2)
@@ -67,6 +86,12 @@ class MockingSpec extends Specification {
         0 * warehouse._
     }
 
+    /**
+     * <div style="font-size: 30;background:#3c423f"><div style="font-size: 35;">&#128142 Using Mocks</div>
+     * <br><ul>
+     * <li>Stubbing objects with Groovy's closures</li>
+     * </ul>
+     */
     def "vip status is correctly passed to credit card"() {
         given: "camera"
         Product camera = new Product(name: "panasonic", price: 350, weight: 2)
